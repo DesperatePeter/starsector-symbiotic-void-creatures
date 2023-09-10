1ba68c672f589215429bf83f79d90fd7a0d281bb
DesperatePeter <jannes@eckart-ahrensburg.de>
2023-09-10 10:41:13 +0200

    feat: add option to hold ctrl to fill up fuel tank using whale oil

--- src/tecrys/svc/items/StjarwhaleOilItemPlugin.kt
+++ src/tecrys/svc/items/StjarwhaleOilItemPlugin.kt
@@ -1,6 +1,7 @@
 package tecrys.svc.items
 
 import com.fs.starfarer.api.campaign.impl.items.BaseSpecialItemPlugin
+import org.lwjgl.input.Keyboard
 
 class StjarwhaleOilItemPlugin : BaseSpecialItemPlugin() {
     companion object{
@@ -9,7 +10,11 @@ class StjarwhaleOilItemPlugin : BaseSpecialItemPlugin() {
     override fun hasRightClickAction(): Boolean = true
     override fun shouldRemoveOnRightClickAction(): Boolean = false
     override fun performRightClickAction() {
-        stack.subtract(1f)
-        stack.cargo.addFuel(FUEL_PER_OIL)
+        do {
+            stack.subtract(1f)
+            stack.cargo.addFuel(FUEL_PER_OIL)
+        }while ((Keyboard.isKeyDown(Keyboard.KEY_LCONTROL) || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL) || Keyboard.isKeyDown(Keyboard.KEY_RMETA) || Keyboard.isKeyDown(Keyboard.KEY_LMETA))
+            && stack.size >= 1f && stack.cargo.freeFuelSpace.toFloat() >= FUEL_PER_OIL )
+
     }
 }
