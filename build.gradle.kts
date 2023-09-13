b762b589065d9c68491c80221819dee8fdfd7a9a
Tecrys <133601087+Tecrys@users.noreply.github.com>
2023-09-13 23:26:50 +0200

    feat: prepare release 0.1.1-alpha

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ val modFolderName = modName.replace(" ", "-")
 /** Set below to `true` to automatically create mod_info.json and Version Checker files. */
 val shouldAutomaticallyCreateMetadataFiles = true
 // Then, if above is set to true, update the rest of the information below in SECTION B.
-val modVersion = "0.1.1"
+val modVersion = "0.1.2"
 val jarFileName = "${modName.replace(" ", "-")}.jar"
 val modId = "symbiotic_void_creatures"
 val modAuthor = "Tecrys & DesperatePeter"
