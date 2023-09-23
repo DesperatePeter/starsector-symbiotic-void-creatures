b2842fb37fa0ebd2e6d7c5098da33b398374439d
DesperatePeter <jannes@eckart-ahrensburg.de>
2023-09-23 15:19:09 +0200

    feat: adjust erractic propulsion values

--- src/tecrys/svc/hullmods/ErraticPropulsion.kt
+++ src/tecrys/svc/hullmods/ErraticPropulsion.kt
@@ -9,13 +9,13 @@ import java.awt.Color
 class ErraticPropulsion: BaseHullMod() {
     companion object{
         const val INTERVAL_MIN = 1.5f
-        const val INTERVAL_MAX = 3f
+        const val INTERVAL_MAX = 4.5f
         const val BUFF_ID = "svc_erratic_propulsion"
         val addedSpeedBuffByRoll = mapOf(
             0.2f to 0.25f, // when roll below 0.2f, add 0.25f
-            0.1f to 0.25f, // when roll below 0.1f, add additional 0.25f for total of 0.5f
-            0.05f to 0.5f,
-            0.01f to 1.5f
+            0.1f to 0.15f, // when roll below 0.1f, add additional 0.15f for total of 0.4f
+            0.05f to 0.35f,
+            0.01f to 0.5f
         )
     }
 
@@ -34,8 +34,9 @@ class ErraticPropulsion: BaseHullMod() {
             addedSpeedBuffByRoll.forEach {
                 if(roll <= it.key) speedBuff += it.value
             }
-            if(speedBuff >= 2f){
-                Global.getCombatEngine().addFloatingText(ship.location, "Burst of Speed!", 20f, Color.BLUE, ship, 1f, 3f)
+            if(speedBuff >= 1.5f){
+                val col = if(speedBuff >= 2f) Color.RED else Color.BLUE
+                Global.getCombatEngine().addFloatingText(ship.location, "Burst of Speed!", 20f, col, ship, 1f, 3f)
             }
             ship.mutableStats?.run {
                 listOf(maxSpeed, acceleration, turnAcceleration).forEach {
