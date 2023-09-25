d2e36e5374f257aa2bf3a7d0db99f06bc9d8e9bb
DesperatePeter <jannes@eckart-ahrensburg.de>
2023-09-25 22:44:13 +0200

    feat: make VoidChitinPlating give the ship the parry defense system

--- src/tecrys/svc/plugins/exotica/VoidChitinPlating.kt
+++ src/tecrys/svc/plugins/exotica/VoidChitinPlating.kt
@@ -1,10 +1,14 @@
 package tecrys.svc.plugins.exotica
 
+import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.campaign.CampaignFleetAPI
 import com.fs.starfarer.api.campaign.CargoAPI
 import com.fs.starfarer.api.campaign.SpecialItemData
 import com.fs.starfarer.api.campaign.econ.MarketAPI
 import com.fs.starfarer.api.combat.MutableShipStatsAPI
+import com.fs.starfarer.api.combat.ShieldAPI
+import com.fs.starfarer.api.combat.ShipAPI
+import com.fs.starfarer.api.combat.ShipVariantAPI
 import com.fs.starfarer.api.fleet.FleetMemberAPI
 import com.fs.starfarer.api.ui.TooltipMakerAPI
 import com.fs.starfarer.api.ui.UIComponentAPI
@@ -14,6 +18,8 @@ import exoticatechnologies.modifications.exotics.ExoticData
 import exoticatechnologies.util.Utilities
 import org.json.JSONObject
 import tecrys.svc.VOID_CHITIN_ID
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType
 
 class VoidChitinPlating(key: String, settings: JSONObject) : Exotic(key, settings) {
     companion object{
@@ -31,6 +37,51 @@ class VoidChitinPlating(key: String, settings: JSONObject) : Exotic(key, setting
         return true
     }
 
+    override fun canApply(member: FleetMemberAPI, variant: ShipVariantAPI, mods: ShipModifications?): Boolean {
+        return variant.hullSpec.defenseType != ShieldAPI.ShieldType.NONE
+    }
+
+    override fun canDropFromFleets(): Boolean = false
+
+    override fun getCannotApplyReasons(member: FleetMemberAPI, mods: ShipModifications?): List<String> {
+        val toReturn = mutableListOf<String>()
+        if(member.variant.hullSpec.defenseType == ShieldAPI.ShieldType.NONE){
+            toReturn.add("Cannot be added to ships with no defensive system")
+        }
+        return toReturn
+    }
+
+    override fun onInstall(member: FleetMemberAPI) {
+
+        member.hullSpec.shipDefenseId = "parry"
+        val spec = member.hullSpec.shieldSpec
+        val methodClass = Class.forName("java.lang.reflect.Method", false, Class::class.java.classLoader)
+        val invokeMethod = MethodHandles.lookup().findVirtual(methodClass,
+            "invoke", MethodType.methodType(Any::class.java, Any::class.java, Array<Any>::class.java))
+
+        var foundMethod: Any? = null
+
+        for (method in spec::class.java.declaredMethods as Array<Any>)
+        {
+            if (method.toString().contains("setType"))
+            {
+                foundMethod = method
+            }
+        }
+        invokeMethod.invoke(foundMethod, spec, ShieldAPI.ShieldType.PHASE)
+
+    }
+
+    override fun applyToShip(
+        id: String,
+        member: FleetMemberAPI,
+        ship: ShipAPI,
+        mods: ShipModifications,
+        exoticData: ExoticData
+    ) {
+        // ship.shield.type = ShieldAPI.ShieldType.PHASE
+    }
+
     override fun applyExoticToStats(
         id: String,
         stats: MutableShipStatsAPI,
@@ -39,8 +90,8 @@ class VoidChitinPlating(key: String, settings: JSONObject) : Exotic(key, setting
         exoticData: ExoticData
     ) {
         // TODO (the current code is just here to make it REALLY obvious for testing if the exotic is installed)
-        stats.armorDamageTakenMult.modifyMult(id, 0.01f)
-        stats.hullDamageTakenMult.modifyMult(id, 0.01f)
+        // stats.armorDamageTakenMult.modifyMult(id, 0.01f)
+        // stats.hullDamageTakenMult.modifyMult(id, 0.01f)
     }
 
     override fun modifyToolTip(
