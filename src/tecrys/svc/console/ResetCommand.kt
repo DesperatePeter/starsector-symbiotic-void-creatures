efe8f24340054c3bc6f26dc781c2bda536d422e9
DesperatePeter <jannes@eckart-ahrensburg.de>
2023-10-07 15:35:04 +0200

    feat: add reset console command

---
+++ src/tecrys/svc/console/ResetCommand.kt
@@ -0,0 +1,28 @@
+package tecrys.svc.console
+
+import com.fs.starfarer.api.Global
+import org.lazywizard.console.BaseCommand
+import tecrys.svc.SVC_FLEET_DEFEATED_MEM_KEY
+import tecrys.svc.defeatedHunterFleets
+import tecrys.svc.internalWhaleReputation
+import tecrys.svc.rulecmd.SvcShouldSpawnHunterBarEvent
+import tecrys.svc.rulecmd.SvcShouldSpawnHuntersDefeatedBarEvent
+import tecrys.svc.world.fleets.FleetManager
+import tecrys.svc.world.fleets.FleetSpawnParameterCalculator
+import tecrys.svc.world.fleets.hunterFleetsById
+import tecrys.svc.world.fleets.hunterFleetsToSpawn
+
+class ResetCommand: BaseCommand {
+    override fun runCommand(args: String, ctx: BaseCommand.CommandContext): BaseCommand.CommandResult {
+        if(ctx != BaseCommand.CommandContext.CAMPAIGN_MAP) return BaseCommand.CommandResult.WRONG_CONTEXT
+        hunterFleetsToSpawn = hunterFleetsById.toMutableMap()
+        defeatedHunterFleets = 0
+        internalWhaleReputation = 100f
+        SvcShouldSpawnHuntersDefeatedBarEvent.hasAlreadyTriggered = false
+        SvcShouldSpawnHunterBarEvent.hasAlreadyTriggered = false
+        FleetManager.whaleSpawnIntervalMultiplier = 1f
+        FleetSpawnParameterCalculator.extraSpawnPower = 0f
+        Global.getSector().memory.unset(SVC_FLEET_DEFEATED_MEM_KEY)
+        return BaseCommand.CommandResult.SUCCESS
+    }
+}
