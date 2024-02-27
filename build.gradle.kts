bada7e29da7cb1c45a068833511f21d1461da80b
Tecrys <133601087+Tecrys@users.noreply.github.com>
2024-02-27 19:27:58 +0100

    feat: prepare release 0.5.0-alpha

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val modName = "SymbioticVoidCreatures"
  * the path behind the ?:.
  * Note: On Linux, if you installed Starsector into your home directory, you have to write /home/<user>/ instead of ~/
  */
-val starsectorDirectory = if(providers.gradleProperty("starsector.dir").isPresent) providers.gradleProperty("starsector.dir").get() else "F:/Games/Starsector0.96a"
+val starsectorDirectory = if(providers.gradleProperty("starsector.dir").isPresent) providers.gradleProperty("starsector.dir").get() else "C:/Games/Starsector0.97a"
 
 /** Defaults to the name of your mod, with spaces replaced by hyphens. */
 val modFolderName = modName.replace(" ", "-")
