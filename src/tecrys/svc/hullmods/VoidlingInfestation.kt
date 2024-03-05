625d8688078723cc13a9c2eb3b9dc75eef0c8f18
DesperatePeter <jannes@eckart-ahrensburg.de>
2024-03-05 18:05:18 +0100

    fix: issues with VoidlingInfestation

--- src/tecrys/svc/hullmods/VoidlingInfestation.kt
+++ src/tecrys/svc/hullmods/VoidlingInfestation.kt
@@ -1,25 +1,37 @@
 package tecrys.svc.hullmods
 
 import com.fs.starfarer.api.Global
+import com.fs.starfarer.api.combat.BaseHullMod
 import com.fs.starfarer.api.combat.ShipAPI
 
-class VoidlingInfestation: BiologicalBaseHullmod() {
+class VoidlingInfestation: BaseHullMod() {
     companion object{
         const val TRIGGER_HULL_LEVEL = 0.5f
         const val FIGHTER_ID = "svc_mios_mandibles_wing"
-        const val NUMBER_OF_FIGHTERS = 3
+        val NUMBER_OF_FIGHTERS = mapOf(
+            ShipAPI.HullSize.FIGHTER to 1,
+            ShipAPI.HullSize.FRIGATE to 2,
+            ShipAPI.HullSize.DESTROYER to 4,
+            ShipAPI.HullSize.CRUISER to 7,
+            ShipAPI.HullSize.CAPITAL_SHIP to 9
+        )
     }
 
-    private val alreadyTriggered = mutableListOf<String>()
+    private val alreadyTriggered = mutableSetOf<String>()
     override fun advanceInCombat(ship: ShipAPI?, amount: Float) {
         val id = ship?.id ?: return
         if(id in alreadyTriggered) return
-        if(ship.hullLevel <= TRIGGER_HULL_LEVEL) spawnFighters(ship)
+        if(ship.hullLevel <= TRIGGER_HULL_LEVEL){
+            spawnFighters(ship)
+            alreadyTriggered.add(ship.id)
+        }
     }
 
     private fun spawnFighters(ship: ShipAPI){
-        for(i in 0 until NUMBER_OF_FIGHTERS){
-            Global.getCombatEngine().getFleetManager(ship.originalOwner).spawnShipOrWing(FIGHTER_ID, ship.location, ship.facing + 30f * i.toFloat())
+        for(i in 0 until (NUMBER_OF_FIGHTERS[ship.hullSize] ?: 0)){
+            Global.getCombatEngine().getFleetManager(ship.originalOwner).spawnShipOrWing(
+                FIGHTER_ID, ship.location, ship.facing + 30f * i.toFloat()
+            )
         }
     }
 }
