432e1556ccdd910533a8154d25e89a772e4223ae
DesperatePeter <jannes@eckart-ahrensburg.de>
2024-03-06 20:38:25 +0100

    feat: add impl for shell vulcanization

---
+++ src/tecrys/svc/hullmods/ShellVulcanization.kt
@@ -0,0 +1,34 @@
+package tecrys.svc.hullmods
+
+import com.fs.starfarer.api.combat.MutableShipStatsAPI
+import com.fs.starfarer.api.combat.ShipAPI
+import tecrys.svc.shipsystems.Parry
+
+class ShellVulcanization: BiologicalBaseHullmod() {
+
+    companion object{
+        const val PARRY_DURATION_BUFF_MULT = 1.5f
+        const val ARMOR_DAMAGE_MULTIPLIER = 1.25f
+        const val HULLMOD_ID = "svc_shell_vulcanization"
+    }
+
+    override fun applyEffectsBeforeShipCreation(hullSize: ShipAPI.HullSize?, stats: MutableShipStatsAPI?, id: String?) {
+        stats?.armorDamageTakenMult?.modifyMult(HULLMOD_ID, ARMOR_DAMAGE_MULTIPLIER)
+    }
+
+    override fun isApplicableToShip(ship: ShipAPI?): Boolean {
+        return super.isApplicableToShip(ship) && ship?.hullSpec?.shipDefenseId == Parry.SYSTEM_ID
+    }
+
+    override fun getUnapplicableReason(ship: ShipAPI?): String {
+        return "Can only be applied to biological ships with the parry defense system."
+    }
+
+    override fun getDescriptionParam(index: Int, hullSize: ShipAPI.HullSize?): String? {
+        return when(index){
+            0 -> "${(PARRY_DURATION_BUFF_MULT * 100f - 100f).toInt()}%" // increases parry duration by s%
+            1 -> "${(ARMOR_DAMAGE_MULTIPLIER * 100f - 100f).toInt()}%" // increases armor damage taken by s%
+            else -> null
+        }
+    }
+}
--- src/tecrys/svc/shipsystems/Parry.kt
+++ src/tecrys/svc/shipsystems/Parry.kt
@@ -1,15 +1,13 @@
 package tecrys.svc.shipsystems
 
 import com.fs.starfarer.api.Global
-import com.fs.starfarer.api.combat.CollisionClass
-import com.fs.starfarer.api.combat.DamagingProjectileAPI
-import com.fs.starfarer.api.combat.MutableShipStatsAPI
-import com.fs.starfarer.api.combat.ShipAPI
+import com.fs.starfarer.api.combat.*
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript
 import com.fs.starfarer.api.plugins.ShipSystemStatsScript
 import org.lazywizard.lazylib.combat.CombatUtils
 import org.lwjgl.util.vector.Vector2f
 import org.magiclib.kotlin.setAlpha
+import tecrys.svc.hullmods.ShellVulcanization
 import tecrys.svc.utils.degToRad
 import java.awt.Color
 import kotlin.math.PI
@@ -19,10 +17,12 @@ import kotlin.math.sin
 class Parry: BaseShipSystemScript() {
     companion object{
         const val RANGE = 150f
+        const val SYSTEM_ID = "parry"
     }
 
     private val affectedProjectiles = mutableSetOf<DamagingProjectileAPI>()
     private var afterImageShown = false
+    private var wasDurationExtended = false
 
     override fun apply(
         stats: MutableShipStatsAPI?,
@@ -32,22 +32,14 @@ class Parry: BaseShipSystemScript() {
     ) {
         val ship = stats?.entity as? ShipAPI ?: return
         if(!afterImageShown){
-
-            val sys = Global.getSettings().getShipSystemSpec("parry")
-            val duration = sys?.active ?: 0.3f
-            val buildup = sys?.`in` ?: 0.1f
-            val down = sys?.out ?: 0.1f
-            var col = Color(70, 10, 200, 150)
-            for(i in 0..3){
-                val angle = (ship.facing + 150f + Math.random().toFloat() * 60f) * degToRad
-                val aaLoc = Vector2f(cos(angle) * 15f * i.toFloat(),  sin(angle) * 15f * i.toFloat())
-                col = col.setAlpha(col.alpha - 30)
-                ship.addAfterimage(col, aaLoc.x, aaLoc.y, ship.velocity.x * 0.05f * i.toFloat(), ship.velocity.y * 0.05f * i.toFloat(), 0.2f, buildup, duration, down, false, true, true)
-            }
-
+            showAfterImage(ship)
             afterImageShown = true
         }
         if(state != ShipSystemStatsScript.State.ACTIVE) return
+        if(isExtendedDuration(ship) && !wasDurationExtended && effectLevel >= ShellVulcanization.PARRY_DURATION_BUFF_MULT - 1f){
+            ship.phaseCloak?.forceState(ShipSystemAPI.SystemState.ACTIVE, 0f);
+            wasDurationExtended = true
+        }
         val projectiles = CombatUtils.getProjectilesWithinRange(ship.location, ship.collisionRadius + RANGE)
         val missiles = CombatUtils.getMissilesWithinRange(ship.location, ship.collisionRadius + RANGE).filter {
             !it.isGuided
@@ -77,9 +69,28 @@ class Parry: BaseShipSystemScript() {
         }
     }
 
+    private fun showAfterImage(ship: ShipAPI){
+        val sys = Global.getSettings().getShipSystemSpec(SYSTEM_ID)
+        val duration = sys?.active ?: 0.3f
+        val buildup = sys?.`in` ?: 0.1f
+        val down = sys?.out ?: 0.1f
+        var col = Color(70, 10, 200, 150)
+        for(i in 0..3){
+            val angle = (ship.facing + 150f + Math.random().toFloat() * 60f) * degToRad
+            val aaLoc = Vector2f(cos(angle) * 15f * i.toFloat(),  sin(angle) * 15f * i.toFloat())
+            col = col.setAlpha(col.alpha - 30)
+            ship.addAfterimage(col, aaLoc.x, aaLoc.y, ship.velocity.x * 0.05f * i.toFloat(), ship.velocity.y * 0.05f * i.toFloat(), 0.2f, buildup, duration, down, false, true, true)
+        }
+    }
+
+    private fun isExtendedDuration(ship: ShipAPI): Boolean{
+        return ship.variant?.hasHullMod(ShellVulcanization.HULLMOD_ID) == true
+    }
+
     override fun unapply(stats: MutableShipStatsAPI?, id: String?) {
         super.unapply(stats, id)
         affectedProjectiles.clear()
         afterImageShown = false
+        wasDurationExtended = false
     }
 }
