e53a50e0f4d167f3d3497ad95d6d9cedfd3212ae
DesperatePeter <jannes@eckart-ahrensburg.de>
2024-03-16 10:37:49 +0100

    feat: adjust whale interaction plugin to provide oil on feeding

--- src/tecrys/svc/world/notifications/NeutralWhaleFleetInteraction.kt
+++ src/tecrys/svc/world/notifications/NeutralWhaleFleetInteraction.kt
@@ -1,19 +1,15 @@
 package tecrys.svc.world.notifications
 
 import com.fs.starfarer.api.Global
-import com.fs.starfarer.api.campaign.CampaignFleetAPI
-import com.fs.starfarer.api.campaign.OptionPanelAPI
-import com.fs.starfarer.api.campaign.SectorEntityToken
-import com.fs.starfarer.api.fleet.FleetAPI
-import org.lwjgl.input.Keyboard
+import com.fs.starfarer.api.campaign.*
 import tecrys.svc.SVC_NOTIFICATIONS_CATEGORY_TEXT_KEY
+import tecrys.svc.WHALE_OIL_ITEM_ID
 import tecrys.svc.WHALE_REPUTATION_MIN
 import tecrys.svc.internalWhaleReputation
 import tecrys.svc.utils.addLeaveOption
 import tecrys.svc.world.fleets.FleetManager
-import kotlin.math.truncate
 
-class NeutralWhaleFleetInteraction(private val whales: SectorEntityToken): NotificationDialogBase(
+class NeutralWhaleFleetInteraction(private val whales: SectorEntityToken) : NotificationDialogBase(
 
 
     kotlin.run {
@@ -25,20 +21,27 @@ class NeutralWhaleFleetInteraction(private val whales: SectorEntityToken): Notif
     Global.getSettings().getSpriteName("backgrounds", "whale_encounter")
 ) {
 
-    companion object{
+    companion object {
         const val FEED_AMOUNT = 200f
+        const val REPUTATION_GAIN_FEED = 25f
+        const val REPUTATION_GAIN_LEAVE = 1f
+        const val MIN_REPUTATION_FOR_OIL_FROM_FEEDING = 100f
+        const val OIL_GAIN_REPUTATION_CAP = 250f
+        const val MAX_OIL_GAINED_FROM_FEEDING = 100f
     }
 
     override fun addOptions(options: OptionPanelAPI) {
-        if (internalWhaleReputation > WHALE_REPUTATION_MIN){
+        if (internalWhaleReputation > WHALE_REPUTATION_MIN) {
             Global.getSector().playerFleet?.cargo?.let { playerCargo ->
                 options.addOption("Feed the whales", "Feed")
 
                 val food = playerCargo.getCommodityQuantity("food")
                 val canFeed = playerCargo.supplies > FEED_AMOUNT || food > FEED_AMOUNT
                 options.setEnabled("Feed", canFeed)
-                options.setTooltip("Feed", "Feed whales with $FEED_AMOUNT food or supplies." +
-                        " Will use ${if(food > FEED_AMOUNT) "food" else "supplies."}")
+                options.setTooltip(
+                    "Feed", "Feed whales with $FEED_AMOUNT food or supplies." +
+                            " Will use ${if (food > FEED_AMOUNT) "food" else "supplies."}"
+                )
             }
         }
         options.addOption("Slaughter the whales to harvest oil.", "Slaughter")
@@ -47,13 +50,13 @@ class NeutralWhaleFleetInteraction(private val whales: SectorEntityToken): Notif
     }
 
     override fun optionSelected(optionText: String?, optionData: Any?) {
-        when(optionData as? String){
+        when (optionData as? String) {
             null -> return
             "Feed" -> feedWhales()
             "Slaughter" -> ProtectedWhalesDialog.slaughterWhales(whales as? CampaignFleetAPI)
             "Leave" -> {
-                if (internalWhaleReputation < WHALE_REPUTATION_MIN){
-                    internalWhaleReputation += 1f
+                if (internalWhaleReputation < WHALE_REPUTATION_MIN) {
+                    internalWhaleReputation += REPUTATION_GAIN_LEAVE
                     Global.getSector().campaignUI?.addMessage("The whales seem to become a little more relaxed before disappearing into the void!")
                 }
             }
@@ -63,21 +66,27 @@ class NeutralWhaleFleetInteraction(private val whales: SectorEntityToken): Notif
         dialog?.dismiss()
     }
 
-    private fun feedWhales(){
+    private fun feedWhales() {
         val playerCargo = Global.getSector().playerFleet?.cargo ?: return
-        if(playerCargo.getCommodityQuantity("food") > FEED_AMOUNT){
+        if (playerCargo.getCommodityQuantity("food") > FEED_AMOUNT) {
             playerCargo.removeCommodity("food", FEED_AMOUNT)
             Global.getSector().campaignUI?.addMessage("Lost 200 food!")
-        }else{
+        } else {
             playerCargo.removeSupplies(FEED_AMOUNT)
             Global.getSector().campaignUI?.addMessage("Lost 200 supplies!")
         }
-        val whale = (whales as? CampaignFleetAPI)?.membersWithFightersCopy?.random()
-        whale?.let { wh ->
-            Global.getSector().playerFleet.fleetData.addFleetMember(wh)
-            Global.getSector().campaignUI?.addMessage("${wh.shipName} joined your fleet!")
-            internalWhaleReputation += 5f
+
+        val oilRepFactor = (Math.random().toFloat() * (internalWhaleReputation - MIN_REPUTATION_FOR_OIL_FROM_FEEDING) /
+                (OIL_GAIN_REPUTATION_CAP - MIN_REPUTATION_FOR_OIL_FROM_FEEDING)).coerceIn(0f, 1f)
+        val oilQuantity = oilRepFactor * MAX_OIL_GAINED_FROM_FEEDING
+        if(oilQuantity > 0f){
+            val oilItem = SpecialItemData(WHALE_OIL_ITEM_ID, WHALE_OIL_ITEM_ID)
+            playerCargo.addItems(CargoAPI.CargoItemType.SPECIAL, oilItem, oilQuantity)
+            Global.getSector().campaignUI?.addMessage("Thankful for the food, the whales leave you a present: " +
+                    "Gained $oilQuantity Stjarwhale Oil")
         }
+
+        internalWhaleReputation += REPUTATION_GAIN_FEED
         Global.getSector().campaignUI?.addMessage("The whales disappear into the void, thankful for the food!")
     }
 }
