10bc3c9cd2601007d26aa866353f0da392a4273f
DesperatePeter <jannes@eckart-ahrensburg.de>
2024-07-06 13:33:47 +0200

    feat: add svc battle theme to combats against voidlings

--- src/tecrys/svc/CombatPlugin.kt
+++ src/tecrys/svc/CombatPlugin.kt
@@ -1,9 +1,11 @@
 package tecrys.svc
 
+import com.fs.starfarer.api.GameState
 import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin
 import com.fs.starfarer.api.combat.ViewportAPI
-import com.fs.starfarer.api.graphics.SpriteAPI
+import com.fs.starfarer.api.input.InputEventAPI
+import com.fs.starfarer.api.mission.FleetSide
 import org.lazywizard.lazylib.opengl.DrawUtils
 import org.lwjgl.util.vector.Vector2f
 import org.magiclib.kotlin.setAlpha
@@ -11,12 +13,12 @@ import tecrys.svc.utils.postRender
 import tecrys.svc.utils.preRender
 import tecrys.svc.utils.setColor
 import java.awt.Color
-import kotlin.math.PI
 import kotlin.math.sin
 
 class CombatPlugin : BaseEveryFrameCombatPlugin() {
 
     private var pulseTimer = 0f
+    private var wasFirstSuccessfulAdvanceCall = false
     companion object{
         data class AuraInfo(val center: Vector2f, val radius: Float, val color: Color)
 
@@ -25,7 +27,23 @@ class CombatPlugin : BaseEveryFrameCombatPlugin() {
         private fun multiplyAlpha(color: Color, mult: Float): Color{
             return color.setAlpha((color.alpha * mult).toInt())
         }
+        const val IS_BATTLE_THEME_PLAYING_MEM_KEY = "\$SVC_BATTLE_MUSIC_PLAYING"
     }
+
+    override fun advance(amount: Float, events: MutableList<InputEventAPI>?) {
+        // skip when on title screen
+        if(Global.getCurrentState() == GameState.TITLE) wasFirstSuccessfulAdvanceCall = true
+        if(!wasFirstSuccessfulAdvanceCall){
+            val enemyFaction = Global.getCombatEngine()?.getFleetManager(FleetSide.ENEMY)?.deployedCopy?.
+            filterNotNull()?.firstOrNull()?.fleetData?.fleet?.faction
+            wasFirstSuccessfulAdvanceCall = enemyFaction != null
+            if(enemyFaction == Global.getSector().getFaction(SVC_FACTION_ID)){
+                 Global.getSoundPlayer().playCustomMusic(1, 1, "svc_voidling_battle_theme", true)
+                Global.getSector().memoryWithoutUpdate[IS_BATTLE_THEME_PLAYING_MEM_KEY] = true
+            }
+        }
+    }
+
     override fun renderInWorldCoords(viewport: ViewportAPI?) {
         if(Global.getCombatEngine()?.isPaused != true) pulseTimer += 0.005f
         if(pulseTimer >= 1000000000f) pulseTimer = 0f
--- src/tecrys/svc/world/ContextBaseMusicPlayer.kt
+++ src/tecrys/svc/world/ContextBaseMusicPlayer.kt
@@ -1,8 +1,10 @@
 package tecrys.svc.world
 
 import com.fs.starfarer.api.EveryFrameScript
+import com.fs.starfarer.api.GameState
 import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.util.IntervalUtil
+import tecrys.svc.CombatPlugin
 import tecrys.svc.SVC_FACTION_ID
 import tecrys.svc.utils.DelayedMusicPlayer
 import tecrys.svc.world.fleets.FleetSpawner
@@ -21,6 +23,8 @@ class ContextBaseMusicPlayer: EveryFrameScript {
     override fun runWhilePaused(): Boolean = false
 
     override fun advance(amount: Float) {
+        if(Global.getCurrentState() != GameState.CAMPAIGN) return
+        stopBattleThemeIfApplicable()
         if(Global.getSector().playerFleet.isInHyperspace) return
         timer.advance(amount)
         if(timer.intervalElapsed()){
@@ -37,5 +41,12 @@ class ContextBaseMusicPlayer: EveryFrameScript {
 
     private fun playSvcTheme(){
         DelayedMusicPlayer.playDelayedMusic("svc_voidling_exploration_theme", 4, 1, false)
+    }
+
+    private fun stopBattleThemeIfApplicable(){
+        if(Global.getSector().memoryWithoutUpdate.getBoolean(CombatPlugin.IS_BATTLE_THEME_PLAYING_MEM_KEY)){
+            Global.getSoundPlayer().pauseCustomMusic()
+            Global.getSector().memoryWithoutUpdate.unset(CombatPlugin.IS_BATTLE_THEME_PLAYING_MEM_KEY)
+        }
     }
 }
