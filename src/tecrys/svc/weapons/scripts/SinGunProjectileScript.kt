89e3cb275cdf533dd6fbaa3bfc5ddec6ffe5610e
DesperatePeter <jannes@eckart-ahrensburg.de>
2025-04-12 18:06:12 +0200

    add sin gun script to weapon

--- src/tecrys/svc/weapons/scripts/SinGunProjectileScript.kt
+++ src/tecrys/svc/weapons/scripts/SinGunProjectileScript.kt
@@ -6,6 +6,7 @@ import com.fs.starfarer.api.combat.DamagingProjectileAPI
 import com.fs.starfarer.api.input.InputEventAPI
 import com.fs.starfarer.api.util.Misc
 import org.lazywizard.lazylib.ext.minus
+import org.lwjgl.util.vector.Vector2f
 import kotlin.math.sin
 
 class SinGunProjectileScript(projs: List<DamagingProjectileAPI>, weaponAngle: Float) : BaseEveryFrameCombatPlugin() {
@@ -13,8 +14,8 @@ class SinGunProjectileScript(projs: List<DamagingProjectileAPI>, weaponAngle: Fl
         const val PHASE_TIME_MULT = 1f
         const val INNER_PM = 3f
         const val OUTER_PM = 2f
-        const val OUTER_AMPLITUDE = 200f
-        const val INNER_AMPLITUDE = 100f
+        const val OUTER_AMPLITUDE = 2000f
+        const val INNER_AMPLITUDE = 1000f
         const val MULT_NAME = "SvcSinGunDist"
         fun computeDamageMult(amplitude: Float, distance: Float): Float{
             return (2f - distance/amplitude).coerceIn(0f, 2f)
@@ -49,10 +50,8 @@ class SinGunProjectileScript(projs: List<DamagingProjectileAPI>, weaponAngle: Fl
             innerProjectiles.second to Pair(INNER_PM, -INNER_AMPLITUDE)
         ).forEach { (projectile, pmAndAmp) -> // why can't you decompose inner pairs? :(
             val magnitude = sin(phase * pmAndAmp.first) * pmAndAmp.second
-            projectile.velocity.set(
-                projectile.velocity.x + orthVector.x * magnitude,
-                projectile.velocity.y + orthVector.y * magnitude
-            )
+            val deltaV = Vector2f(orthVector.x * magnitude, orthVector.y * magnitude)
+            Vector2f.add(projectile.velocity, deltaV, projectile.velocity)
         }
         val innerDist = (innerProjectiles.first.location - innerProjectiles.second.location).length()
         listOf(innerProjectiles.first, innerProjectiles.second).forEach {
@@ -61,6 +60,11 @@ class SinGunProjectileScript(projs: List<DamagingProjectileAPI>, weaponAngle: Fl
         val outerDist = (outerProjectiles.first.location - outerProjectiles.second.location).length()
         listOf(outerProjectiles.first, outerProjectiles.second).forEach {
             it.damage.modifier.modifyMult(MULT_NAME, computeDamageMult(OUTER_AMPLITUDE, outerDist))
+        }
+        if(projectiles.all {
+            it.isExpired
+            }){
+            Global.getCombatEngine().removePlugin(this)
         }
     }
 }
