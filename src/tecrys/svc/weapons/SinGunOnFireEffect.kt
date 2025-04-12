b172363e7cf3f49f8a073d78642aa95f574e9eab
DesperatePeter <jannes@eckart-ahrensburg.de>
2025-04-12 17:41:10 +0200

    feat: add first untested variant of sin gun script

---
+++ src/tecrys/svc/weapons/SinGunOnFireEffect.kt
@@ -0,0 +1,26 @@
+package tecrys.svc.weapons
+
+import com.fs.starfarer.api.combat.CombatEngineAPI
+import com.fs.starfarer.api.combat.DamagingProjectileAPI
+import com.fs.starfarer.api.combat.OnFireEffectPlugin
+import com.fs.starfarer.api.combat.WeaponAPI
+import tecrys.svc.weapons.scripts.SinGunProjectileScript
+
+class SinGunOnFireEffect: OnFireEffectPlugin {
+    private var looseProjectilesByWeapon = mutableMapOf<WeaponAPI, MutableList<DamagingProjectileAPI>>()
+    override fun onFire(
+        projectile: DamagingProjectileAPI?,
+        weapon: WeaponAPI?,
+        engine: CombatEngineAPI?
+    ) {
+        weapon ?: return
+        projectile ?: return
+        engine ?: return
+        looseProjectilesByWeapon[weapon] ?: kotlin.run { looseProjectilesByWeapon[weapon] = mutableListOf() }
+        looseProjectilesByWeapon[weapon]?.add(projectile)
+        if((looseProjectilesByWeapon[weapon]?.size ?: 0) >= 4 ){
+            engine.addPlugin(looseProjectilesByWeapon[weapon]?.let { SinGunProjectileScript(it, weapon.currAngle) })
+            looseProjectilesByWeapon[weapon] = mutableListOf()
+        }
+    }
+}
