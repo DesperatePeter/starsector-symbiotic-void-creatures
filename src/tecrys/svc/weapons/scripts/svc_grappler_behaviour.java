4d2e2bf426ad9f403db84d66e79853ac02581280
DesperatePeter <jannes@eckart-ahrensburg.de>
2025-04-22 16:09:38 +0200

    fix: grappler formula

--- src/tecrys/svc/weapons/scripts/svc_grappler_behaviour.java
+++ src/tecrys/svc/weapons/scripts/svc_grappler_behaviour.java
@@ -5,41 +5,19 @@ import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI;
 import com.fs.starfarer.api.util.Misc;
 import org.lazywizard.lazylib.MathUtils;
-import tecrys.svc.plugins.GrapplerRopePlugin;
 import org.lwjgl.util.vector.Vector2f;
-//import tecrys.svc.weapons.GreiferEffectBase;
+import tecrys.svc.plugins.GrapplerRopePlugin;
 
 import java.awt.*;
 
-public class svc_grappler_behaviour implements OnFireEffectPlugin, OnHitEffectPlugin{
+public class svc_grappler_behaviour implements OnFireEffectPlugin, OnHitEffectPlugin {
 
-    private Color tentacleColor = new Color(173, 113, 156, 255);
-//    private float pullStrength = 100f;
+    private final Color tentacleColor = new Color(173, 113, 156, 255);
+    private static final float BASE_FORCE = 500f;
+    private static final float FORCE_PER_DISTANCE = 1f;
+    private static final float ESCAPE_VELOCITY_SCALING = 0.5f;
     public static int pluginCount = 0;
-    private float Distance;
-    private float SpeedSource;
-    private float SpeedTarget;
-    private float ShieldRadius;
-//
-//    @Override
-//    public boolean shouldAffectFighters() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean shouldAffectShips() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean shouldAffectObjects() {
-//        return false;
-//    }
-//
-//    @Override
-//    public float computeForceAgainstShip(ShipAPI target, ShipAPI source) {
-//        return Math.max(106f + source.getMass() / 7f, 0.01f);
-//    }
+
     @Override
     public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
         GrapplerRopePlugin plugin = new GrapplerRopePlugin(pluginCount++, 15, 8f, false, weapon, projectile);
@@ -49,92 +27,32 @@ public class svc_grappler_behaviour implements OnFireEffectPlugin, OnHitEffectPl
         projectile.setCustomData("grappler", plugin);
         Global.getCombatEngine().addPlugin(plugin);
 
-
     }
 
     @Override
     public void onHit(DamagingProjectileAPI projectile, CombatEntityAPI target, Vector2f point, boolean shieldHit, ApplyDamageResultAPI damageResult, CombatEngineAPI engine) {
-        Global.getLogger(this.getClass()).info("target: " + target.getClass() );
+        Global.getLogger(this.getClass()).info("target: " + target.getClass());
         GrapplerRopePlugin plugin = (GrapplerRopePlugin) projectile.getCustomData().get("grappler");
         if (target instanceof DamagingProjectileAPI) {
             plugin.kill();
-        }
-        else {
+        } else {
             plugin.attach(target, point, projectile.getFacing());
         }
-        if (projectile.getSource() instanceof ShipAPI && target instanceof ShipAPI) {
-            ShipAPI sourceShip = (ShipAPI) projectile.getSource();
-            ShipAPI targetShip = (ShipAPI) target;
-
+        if (projectile.getSource() != null && target instanceof ShipAPI targetShip) {
+            ShipAPI sourceShip = projectile.getSource();
 
             Vector2f sourceLoc = sourceShip.getLocation();
             Vector2f targetLoc = targetShip.getLocation();
-            ShieldRadius = ((ShipAPI) target).getShieldRadiusEvenIfNoShield();
 
-            Vector2f pullVector = Misc.getUnitVectorAtDegreeAngle(Misc.getAngleInDegrees(sourceLoc, targetLoc));
-            Vector2f pullEnemiesVector = Misc.getUnitVectorAtDegreeAngle(Misc.getAngleInDegrees(sourceLoc, targetLoc));
-            Distance = MathUtils.getDistance(targetLoc, sourceLoc);
+            Vector2f pullDirection = Misc.getUnitVectorAtDegreeAngle(Misc.getAngleInDegrees(sourceLoc, targetLoc));
+            float distance = MathUtils.getDistance(targetLoc, sourceLoc);
 
-           SpeedSource = sourceShip.getVelocity().length();
-           SpeedTarget = targetShip.getVelocity().length();
+            float escapeVelocity = Vector2f.dot(pullDirection, targetShip.getVelocity());
 
-            //pullVector.scale(Math.min(10f + sourceShip.getMass() / 22f, 30f));
+            float force = BASE_FORCE + distance * FORCE_PER_DISTANCE;
+            float deltaV = force / sourceShip.getMass()  + escapeVelocity * ESCAPE_VELOCITY_SCALING;
 
-
-//            if (sourceShip.getHullSize().equals(ShipAPI.HullSize.FRIGATE))
-//            {
-//
-//                if (targetShip.getVelocity().length() >= 120 )
-//                {
-//                    pullVector.scale(Math.max((2000f/sourceShip.getMass()) * (Distance / 50f), 110f));
-//                }
-//                else
-//                {
-                    pullVector.scale(  (1000f/sourceShip.getMass()) * (Distance / 300f) * (12f*SpeedTarget/SpeedSource)  );
-//                }
-//            }
-//            if (sourceShip.getHullSize().equals(ShipAPI.HullSize.DESTROYER))
-//            {
-//                if (targetShip.getVelocity().length() >= 100 )
-//                {
-//                    pullVector.scale(Math.max((2000f/sourceShip.getMass()) * (Distance / 50f), 50f));
-//                }
-//                else
-//                {
-//                    pullVector.scale((1000f / sourceShip.getMass()) * (Distance / 100f));
-//                }
-//            }
-//            if (sourceShip.getHullSize().equals(ShipAPI.HullSize.CRUISER))
-//            {
-//                if (targetShip.getVelocity().length() >= 80 )
-//                {
-//                    pullVector.scale(Math.max((2000f/sourceShip.getMass()) * (Distance / 50f), 50f));
-//                }
-//                else
-//                {
-//                    pullVector.scale((2000f / sourceShip.getMass()) * (Distance / 100f));
-//                }
-//            }
-//            if (sourceShip.getHullSize().equals(ShipAPI.HullSize.CAPITAL_SHIP))
-//            {
-//                if (targetShip.getVelocity().length() >= 90 )
-//                {
-//                    pullVector.scale(Math.max((2000f/sourceShip.getMass()) * (Distance / 50f), 40f));
-//                }
-//                else
-//                {
-//                    pullVector.scale((1000f / sourceShip.getMass()) * (Distance / 100f));
-//                }
-//            }
-
-
-
-            //pullEnemiesVector.scale((Math.max(56f - sourceShip.getMass() / 20f, 15f)) * 0.7f);
-
-            sourceShip.getVelocity().translate(pullVector.x, pullVector.y);
-//            targetShip.getVelocity().translate(-pullEnemiesVector.x, -pullEnemiesVector.y);
-
+            sourceShip.getVelocity().translate(pullDirection.x * deltaV, pullDirection.y * deltaV);
         }
     }
-
 }
