9aaaa5b91410973fbbba8bab1681aceb5a5f7295
Tecrys <133601087+Tecrys@users.noreply.github.com>
2025-05-14 20:47:21 +0200

    feat: cleanup after debugging

--- src/tecrys/svc/world/SectorGen.kt
+++ src/tecrys/svc/world/SectorGen.kt
@@ -4,10 +4,8 @@ import com.fs.starfarer.api.campaign.RepLevel
 import com.fs.starfarer.api.campaign.SectorAPI
 import com.fs.starfarer.api.campaign.SectorGeneratorPlugin
 import org.magiclib.util.MagicSettings
-import tecrys.svc.MAGIC_SETTINGS_MOD_KEY
-import tecrys.svc.MAGIC_SETTINGS_RELATIONS_KEY
-import tecrys.svc.SVC_FACTION_ID
-import tecrys.svc.VWL_FACTION_ID
+import tecrys.svc.*
+import tecrys.svc.SvcBasePlugin.Companion
 
 class SectorGen : SectorGeneratorPlugin {
     companion object {
@@ -20,15 +18,18 @@ class SectorGen : SectorGeneratorPlugin {
         sector?.run {
             val svc = getFaction(SVC_FACTION_ID)
             allFactions.filterNotNull().filterNot {
-                ignoredFactions.contains(it.id)
+                SvcBasePlugin.ignoredFactions.contains(it.id)
             }.forEach {
-                svc.setRelationship(it.id, RepLevel.HOSTILE)
-                svc.setRelationship(it.id, RELATIONSHIP_TO_SET)
+                svc.setRelationship(it.id, RepLevel.VENGEFUL)
+//                    svc.setRelationship(it.id, RELATIONSHIP_TO_SET)
             }
+            svc.setRelationship(svc.id, RepLevel.VENGEFUL)
             val vwl = getFaction(VWL_FACTION_ID)
-            vwl.setRelationship(svc.id, RepLevel.HOSTILE)
-            vwl.setRelationship(svc.id, RELATIONSHIP_TO_SET)
+            vwl.setRelationship(svc.id, RepLevel.VENGEFUL)
+//                vwl.setRelationship(svc.id, RELATIONSHIP_TO_SET)
             vwl.setRelationship("player", RepLevel.FRIENDLY)
+            val mmm = getFaction(MMM_FACTION_ID)
+            mmm.setRelationship("player", RepLevel.VENGEFUL)
         }
     }
 }
